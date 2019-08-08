97c3931e86e37adc75b735db2d12cb3ddf28e95d
kai.zhou <zhoukaisspu@163.com>
2019-08-08 23:00:32 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+rm *.o
+rm *.out
 g++ -c serialPortChannel.cpp  -std=c++11 -pthread
 g++ -c emgParser.cpp -std=c++11 -pthread
 g++ -c parserManager.cpp -std=c++11 -pthread
 g++ -c hardwareChannelManager.cpp -std=c++11 -pthread
-g++ main.cpp -o main.out serialPortChannel.o emgParser.o parserManager.o hardwareChannelManager.o -std=c++11 -pthread
+g++ -c websocket_server_async.cpp -pthread -lboost_system
+g++ main.cpp -o main.out serialPortChannel.o emgParser.o parserManager.o hardwareChannelManager.o websocket_server_async.o -std=c++11 -pthread -lboost_system
